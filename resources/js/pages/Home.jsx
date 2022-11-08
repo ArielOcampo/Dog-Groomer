773cefa43c5e12834ca971561a7693f721a396bf
Ariel Ocampo <arioca_181@hotmail.com>
2022-11-08 18:05:21 -0300

    First commit

---
+++ resources/js/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Layout } from "antd";
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+};
+
+export default Home;
